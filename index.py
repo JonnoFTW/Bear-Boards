62e9ebe3262a51fd8ecaba729bf80807cab3784a
Jonathan Mackenzie <jonmac1@gmail.com>
2011-06-04 03:25:47 +0930

    added junk, messing around with web.py

---
+++ index.py
@@ -0,0 +1,9 @@
+import web
+render = web.template.render('templates/')
+urls = ('/(.*)','index')
+app = web.application(urls,globals())
+class index:
+    def GET(self,name):
+        return render.index(name)#"Hello"
+
+if __name__ == "__main__": app.run()
